859735a6d34e522868958d0a428e980d06fce9a3
Fabio Viappiani <viappidu@gmail.com>
2021-01-03 17:58:24 +0100

    fark X_MIN_POS = -15 and Y_MIN_POS = -35git add .!

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1180,8 +1180,8 @@
 #define Y_BED_SIZE 240
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -15
-#define Y_MIN_POS -35
+#define X_MIN_POS 0
+#define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
