7b49876315f34473995903b8f661b00b27b7079d
Fabio Viappiani <viappidu@gmail.com>
2021-04-04 13:58:10 +0200

    thermistor set to 11 (Amazon product description) and Z NOZZLE_TO_PROBE_OFFSET to 0

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -409,7 +409,7 @@
  *   998 : Dummy Table that ALWAYS reads 25°C or the temperature defined below.
  *   999 : Dummy Table that ALWAYS reads 100°C or the temperature defined below.
  */
-#define TEMP_SENSOR_0 1
+#define TEMP_SENSOR_0 11
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
@@ -1064,7 +1064,9 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -28.80, 11.50, -0.7 }
+// #define NOZZLE_TO_PROBE_OFFSET { -28.80, 11.50, -0.7 }
+//TODO LASER #define NOZZLE_TO_PROBE_OFFSET { -28.80, 11.50, -8.50 }
+#define NOZZLE_TO_PROBE_OFFSET { -28.80, 11.50, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
